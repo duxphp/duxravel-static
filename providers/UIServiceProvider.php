bc89ed9529798d2e6d4b5fafc4e0d4f76e2b2ea7
Mr.L <admin@duxphp.com>
2021-06-24 19:17:33 +0800

    update

---
+++ providers/UIServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Duxravel\UI\Providers;
+
+use Duxravel\Core\Util\Hook;
+use Duxravel\Core\Util\Menu;
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class UIServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        $this->publishes([
+            __DIR__.'/../dist' => public_path('static/system'),
+        ], 'duxravel-system');
+    }
+}
