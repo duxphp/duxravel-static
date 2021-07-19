930b2a78ad660a79cf1e70d2eefc9c7b56270070
Mr.L <admin@duxphp.com>
2021-07-19 11:47:23 +0800

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,11 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.postCss('src/css/base.pcss', 'dist/css', [
+
+const srcDir = 'src';
+const distDir = 'dist';
+
+mix.postCss(srcDir + '/css/base.pcss', distDir + '/css', [
     require('postcss-import'),
     tailwindcss('./tailwind.config.js'),
     require('postcss-nesting'),
@@ -10,38 +14,38 @@ mix.postCss('src/css/base.pcss', 'dist/css', [
     .options({
         processCssUrls: false,
     });
-mix.minify(['dist/css/base.css']);
+mix.minify([distDir + '/css/base.css']);
 
 mix.scripts([
-    'src/js/do.js',
-    'src/js/package.js',
-    'src/js/common.js',
-    'src/js/component/app.js',
-    'src/js/component/notify.js',
-    'src/js/component/dialog.js',
-    'src/js/component/table.js',
-    'src/js/component/form.js',
-    'src/js/component/show.js',
-    'src/js/component/system.js',
-    'src/js/component/file.js',
-    'src/js/component/page.js',
-], 'dist/js/app.js');
-mix.minify(['dist/js/app.js']);
+    srcDir + '/js/do.js',
+    srcDir + '/js/package.js',
+    srcDir + '/js/common.js',
+    srcDir + '/js/component/app.js',
+    srcDir + '/js/component/notify.js',
+    srcDir + '/js/component/dialog.js',
+    srcDir + '/js/component/table.js',
+    srcDir + '/js/component/form.js',
+    srcDir + '/js/component/show.js',
+    srcDir + '/js/component/system.js',
+    srcDir + '/js/component/file.js',
+    srcDir + '/js/component/page.js',
+], distDir + '/js/app.js');
+mix.minify([distDir + '/js/app.js']);
 
-mix.copyDirectory('src/img', 'dist/img');
-mix.copyDirectory('src/libs/fontawesome/webfonts', 'dist/webfonts');
-mix.copyDirectory('src/js/package/cascader', 'dist/js/cascader');
-mix.copyDirectory('src/js/package/Inputmask', 'dist/js/Inputmask');
-mix.copyDirectory('src/js/package/pagination', 'dist/js/pagination');
-mix.copyDirectory('src/js/package/password', 'dist/js/password');
-mix.copyDirectory('src/js/package/prompt', 'dist/js/prompt');
-mix.copyDirectory('src/js/package/toast', 'dist/js/toast');
-mix.copyDirectory('src/js/package/tinymce', 'dist/js/tinymce');
-mix.copyDirectory('src/js/package/tinydux/theme/build/ui/dux', 'dist/js/tinymce/skins/ui/dux');
-mix.copyDirectory('src/js/package/tinydux/theme/build/content/dux', 'dist/js/tinymce/skins/content/dux');
+mix.copyDirectory(srcDir + '/img', distDir + '/img');
+mix.copyDirectory(srcDir + '/libs/fontawesome/webfonts', distDir + '/webfonts');
+mix.copyDirectory(srcDir + '/js/package/cascader', distDir + '/js/cascader');
+mix.copyDirectory(srcDir + '/js/package/Inputmask', distDir + '/js/Inputmask');
+mix.copyDirectory(srcDir + '/js/package/pagination', distDir + '/js/pagination');
+mix.copyDirectory(srcDir + '/js/package/password', distDir + '/js/password');
+mix.copyDirectory(srcDir + '/js/package/prompt', distDir + '/js/prompt');
+mix.copyDirectory(srcDir + '/js/package/toast', distDir + '/js/toast');
+mix.copyDirectory(srcDir + '/js/package/tinymce', distDir + '/js/tinymce');
+mix.copyDirectory(srcDir + '/js/package/tinydux/theme/build/ui/dux', distDir + '/js/tinymce/skins/ui/dux');
+mix.copyDirectory(srcDir + '/js/package/tinydux/theme/build/content/dux', distDir + '/js/tinymce/skins/content/dux');
 
-mix.copy('src/libs/fontawesome/css/all.min.css', 'dist/css/fontawesome.min.css');
-mix.copy('src/libs/jquery/jquery-3.5.1.min.js', 'dist/js/jquery.min.js');
-mix.copy('node_modules/@popperjs/core/dist/umd/popper.min.js', 'dist/js/tippy/popper.min.js');
-mix.copy('node_modules/tippy.js/dist/tippy.umd.min.js', 'dist/js/tippy/tippy.min.js');
-mix.copy('node_modules/tippy.js/dist/tippy.css', 'dist/js/tippy/tippy.css');
+mix.copy(srcDir + '/libs/fontawesome/css/all.min.css', distDir + '/css/fontawesome.min.css');
+mix.copy(srcDir + '/libs/jquery/jquery-3.5.1.min.js', distDir + '/js/jquery.min.js');
+mix.copy('node_modules/@popperjs/core/dist/umd/popper.min.js', distDir + '/js/tippy/popper.min.js');
+mix.copy('node_modules/tippy.js/dist/tippy.umd.min.js', distDir + '/js/tippy/tippy.min.js');
+mix.copy('node_modules/tippy.js/dist/tippy.css', distDir + '/js/tippy/tippy.css');
