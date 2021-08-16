e78ca422fa058144d904a7f863b3dbba469736c5
Mr.L <admin@duxphp.com>
2021-08-16 09:16:45 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,13 @@ module.exports = {
     runtimeCompiler: true,
     // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
     productionSourceMap: false,
+
+    chainWebpack: config => {
+        config.optimization.minimize(true) // 开启压缩js代码
+        config.optimization.splitChunks({ // 开启代码分割
+          chunks: 'all'
+        })
+    },
     // 服务
     devServer: {
         // 请求代理
