d53a49f68e3e97d9531775ad0e223c95c534dd55
xujian <908634674@qq.com>
2021-11-05 11:57:38 +0800

    切换

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div :class="{'dark': darkMode === 'dark'}">
-  <n-config-provider
-    :theme="darkTheme"
-    :locale="zhCN"
-    :date-locale="dateZhCN"
-    :theme-overrides="themeApp"
-  >
-    <n-global-style />
-    <n-message-provider>
-      <n-dialog-provider>
-        <n-loading-bar-provider>
-          <n-notification-provider>
-            <Index />
-          </n-notification-provider>
-        </n-loading-bar-provider>
-      </n-dialog-provider>
-    </n-message-provider>
-  </n-config-provider>
+  <div :class="{ dark: darkMode === 'dark' }">
+    <n-config-provider
+      :theme="darkTheme"
+      :locale="zhCN"
+      :date-locale="dateZhCN"
+      :theme-overrides="themeApp"
+    >
+      <n-global-style />
+      <n-message-provider>
+        <n-dialog-provider>
+          <n-loading-bar-provider>
+            <n-notification-provider>
+              <Page :show="show" @switch-dark="switchDark" />
+            </n-notification-provider>
+          </n-loading-bar-provider>
+        </n-dialog-provider>
+      </n-message-provider>
+    </n-config-provider>
   </div>
 </template>
 
 <script>
 import { zhCN, dateZhCN } from "naive-ui";
-import Index from "./page/Index.vue";
+import Page from "./components/Page.vue";
 import { darkTheme } from "naive-ui";
 import themeLight from "./config/themeLight";
 import themeDark from "./config/themeDark";
@@ -30,59 +30,111 @@ import themeDark from "./config/themeDark";
 export default {
   name: "App",
   components: {
-    Index,
+    Page,
   },
   data() {
-    window.derkMode = window.localStorage.getItem('darkMode') === 'dark' ? 'dark' : 'light'
+    window.derkMode =
+      localStorage.getItem("darkMode") === "dark" ? "dark" : "light";
     return {
+      show: true,
       zhCN,
       dateZhCN,
-      themeApp: window.derkMode === 'dark' ? themeDark : themeLight,
-      darkTheme: window.derkMode === 'dark' ? darkTheme : null,
-      darkMode: window.derkMode
+      themeApp: window.derkMode === "dark" ? themeDark : themeLight,
+      darkTheme: window.derkMode === "dark" ? darkTheme : null,
+      darkMode: window.derkMode,
     };
   },
   created() {
-
     // 注册图表
     window.Apex = {
       chart: {
-        locales: [{
-          "name": "zh-CN",
-          "options": {
-            "months": ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-            "shortMonths": ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-            "days": ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
-            "shortDays": ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
-            "toolbar": {
-              "exportToSVG": "下载 SVG",
-              "exportToPNG": "下载 PNG",
-              "exportToCSV": "下载 CSV",
-              "menu": "菜单",
-              "selection": "选择",
-              "selectionZoom": "选择大小",
-              "zoomIn": "放大",
-              "zoomOut": "缩小",
-              "pan": "移动",
-              "reset": "重置"
-            }
-          }
-        }],
-        "fontFamily": "inherit",
-        "defaultLocale": "zh-CN",
+        locales: [
+          {
+            name: "zh-CN",
+            options: {
+              months: [
+                "一月",
+                "二月",
+                "三月",
+                "四月",
+                "五月",
+                "六月",
+                "七月",
+                "八月",
+                "九月",
+                "十月",
+                "十一月",
+                "十二月",
+              ],
+              shortMonths: [
+                "一月",
+                "二月",
+                "三月",
+                "四月",
+                "五月",
+                "六月",
+                "七月",
+                "八月",
+                "九月",
+                "十月",
+                "十一月",
+                "十二月",
+              ],
+              days: [
+                "星期日",
+                "星期一",
+                "星期二",
+                "星期三",
+                "星期四",
+                "星期五",
+                "星期六",
+              ],
+              shortDays: [
+                "周日",
+                "周一",
+                "周二",
+                "周三",
+                "周四",
+                "周五",
+                "周六",
+              ],
+              toolbar: {
+                exportToSVG: "下载 SVG",
+                exportToPNG: "下载 PNG",
+                exportToCSV: "下载 CSV",
+                menu: "菜单",
+                selection: "选择",
+                selectionZoom: "选择大小",
+                zoomIn: "放大",
+                zoomOut: "缩小",
+                pan: "移动",
+                reset: "重置",
+              },
+            },
+          },
+        ],
+        fontFamily: "inherit",
+        defaultLocale: "zh-CN",
       },
       tooltip: {
-        theme: window.derkMode === 'dark' ? 'dark' : 'light'
+        theme: window.derkMode === "dark" ? "dark" : "light",
       },
-      colors: [
-        '#005dff',
-        '#b1cdec',
-        '#00d586'
-      ]
-    }
+      colors: ["#005dff", "#b1cdec", "#00d586"],
+    };
   },
-  method: {
-    modelDialog() {},
+  methods: {
+    switchDark(type) {
+      localStorage.setItem("darkMode", type);
+      this.show = false
+      window.Apex.tooltip.theme = type;
+      window.derkMode = type;
+      this.themeApp = type === "dark" ? themeDark : themeLight;
+      this.darkTheme = type === "dark" ? darkTheme : null;
+      this.darkMode = type;
+      this.$nextTick(() => {
+        this.show = true
+      })
+    },
   },
 };
 </script>
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="flex flex-col lg:flex-row lg:h-screen lg:overflow-hidden">
     <div
-      class="text-white w-16 bg-gray-800 px-2 flex-none flex-col lg:flex hidden border-r border-gray-800 dark:border-gray-700"
+      class="
+        text-white
+        w-16
+        bg-gray-800
+        px-2
+        flex-none flex-col
+        lg:flex
+        hidden
+        border-r border-gray-800
+        dark:border-gray-700
+      "
     >
       <div
         class="
@@ -38,7 +48,10 @@
         </n-dropdown>
       </div>
 
-      <n-layout-content class="app-scrollbar flex-grow bg-gray-800" :native-scrollbar="false">
+      <n-layout-content
+        class="app-scrollbar flex-grow bg-gray-800"
+        :native-scrollbar="false"
+      >
         <div
           v-show="!item.hidden || currentIndexs[0] === index"
           v-for="(item, index) in menu"
@@ -56,10 +69,8 @@
               justify-center
               gap-1
               text-gray-200
-              dark:text-gray-300
-              dark:hover:text-gray-50
-              hover:text-white
-              hover:bg-gray-700
+              dark:text-gray-300 dark:hover:text-gray-50
+              hover:text-white hover:bg-gray-700
               relative
               mb-2
             "
@@ -80,97 +91,267 @@
 
       <div class="flex-none">
         <div
-            class="
-                  cursor-pointer
-                  rounded-sm
-                  py-1.5
-                  text-center
-                  flex
-                  items-center
-                  justify-center
-                  gap-1
-                  hover:text-white
-                  hover:bg-gray-700
-
-
-                  relative
-                  mb-2
-                "
-            @click="switchDark()"
+          class="
+            cursor-pointer
+            rounded-sm
+            py-1.5
+            text-center
+            flex
+            items-center
+            justify-center
+            gap-1
+            hover:text-white hover:bg-gray-700
+            relative
+            mb-2
+          "
+          @click="
+            (darkMode = darkMode === 'dark' ? 'light' : 'dark'),
+              $emit('switch-dark', darkMode)
+          "
         >
-                <span class=" flex items-center justify-center">
-                  <svg v-if="darkMode === 'light'" class="fill-current  text-gray-200" width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M28.0527 4.41085C22.5828 5.83695 18.5455 10.8106 18.5455 16.7273C18.5455 23.7564 24.2436 29.4545 31.2727 29.4545C37.1894 29.4545 42.1631 25.4172 43.5891 19.9473C43.8585 21.256 44 22.6115 44 24C44 35.0457 35.0457 44 24 44C12.9543 44 4 35.0457 4 24C4 12.9543 12.9543 4 24 4C25.3885 4 26.744 4.14149 28.0527 4.41085Z" fill="none" stroke="#eee" stroke-width="4" stroke-linejoin="round"/></svg>
-                  <svg v-if="darkMode === 'dark'" class="fill-current text-gray-200"  width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path fill-rule="evenodd" clip-rule="evenodd" d="M24 3V6.15V3Z" fill="#eee"/><path d="M24 3V6.15" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M38.8492 9.15076L36.6219 11.3781L38.8492 9.15076Z" fill="#eee"/><path d="M38.8492 9.15076L36.6219 11.3781" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M45 24H41.85H45Z" fill="#eee"/><path d="M45 24H41.85" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M38.8492 38.8492L36.6219 36.6219L38.8492 38.8492Z" fill="#eee"/><path d="M38.8492 38.8492L36.6219 36.6219" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M24 45V41.85V45Z" fill="#eee"/><path d="M24 45V41.85" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M9.15076 38.8492L11.3781 36.6219L9.15076 38.8492Z" fill="#eee"/><path d="M9.15076 38.8492L11.3781 36.6219" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M3 24H6.15H3Z" fill="#eee"/><path d="M3 24H6.15" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M9.15076 9.15076L11.3781 11.3781L9.15076 9.15076Z" fill="#eee"/><path d="M9.15076 9.15076L11.3781 11.3781" stroke="#eee" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M24 36C30.6274 36 36 30.6274 36 24C36 17.3726 30.6274 12 24 12C17.3726 12 12 17.3726 12 24C12 30.6274 17.3726 36 24 36Z" fill="none" stroke="#eee" stroke-width="4" stroke-linejoin="round"/></svg>
-
-
-                </span>
+          <span class="flex items-center justify-center">
+            <svg
+              v-if="darkMode === 'light'"
+              class="fill-current text-gray-200"
+              width="16"
+              height="16"
+              viewBox="0 0 48 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+              <path
+                d="M28.0527 4.41085C22.5828 5.83695 18.5455 10.8106 18.5455 16.7273C18.5455 23.7564 24.2436 29.4545 31.2727 29.4545C37.1894 29.4545 42.1631 25.4172 43.5891 19.9473C43.8585 21.256 44 22.6115 44 24C44 35.0457 35.0457 44 24 44C12.9543 44 4 35.0457 4 24C4 12.9543 12.9543 4 24 4C25.3885 4 26.744 4.14149 28.0527 4.41085Z"
+                fill="none"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <svg
+              v-if="darkMode === 'dark'"
+              class="fill-current text-gray-200"
+              width="16"
+              height="16"
+              viewBox="0 0 48 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M24 3V6.15V3Z"
+                fill="#eee"
+              />
+              <path
+                d="M24 3V6.15"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M38.8492 9.15076L36.6219 11.3781L38.8492 9.15076Z"
+                fill="#eee"
+              />
+              <path
+                d="M38.8492 9.15076L36.6219 11.3781"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M45 24H41.85H45Z"
+                fill="#eee"
+              />
+              <path
+                d="M45 24H41.85"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M38.8492 38.8492L36.6219 36.6219L38.8492 38.8492Z"
+                fill="#eee"
+              />
+              <path
+                d="M38.8492 38.8492L36.6219 36.6219"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M24 45V41.85V45Z"
+                fill="#eee"
+              />
+              <path
+                d="M24 45V41.85"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.15076 38.8492L11.3781 36.6219L9.15076 38.8492Z"
+                fill="#eee"
+              />
+              <path
+                d="M9.15076 38.8492L11.3781 36.6219"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M3 24H6.15H3Z"
+                fill="#eee"
+              />
+              <path
+                d="M3 24H6.15"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.15076 9.15076L11.3781 11.3781L9.15076 9.15076Z"
+                fill="#eee"
+              />
+              <path
+                d="M9.15076 9.15076L11.3781 11.3781"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M24 36C30.6274 36 36 30.6274 36 24C36 17.3726 30.6274 12 24 12C17.3726 12 12 17.3726 12 24C12 30.6274 17.3726 36 24 36Z"
+                fill="none"
+                stroke="#eee"
+                stroke-width="4"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </span>
         </div>
       </div>
 
       <div class="flex-none">
-          <n-popover
-            placement="right-end"
-            trigger="click"
-            :show-arrow="false"
-          >
-            <template #trigger>
-              <div
-                class="
-                  cursor-pointer
-                  rounded-sm
-                  py-1.5
-                  text-center
-                  flex
-                  items-center
-                  justify-center
-                  gap-1
-                  hover:text-white
-                  hover:bg-gray-700
-
-                  relative
-                  mb-2
-                "
-              >
-                <span class="w-5 h-6 flex items-center justify-center">
-                  <svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path d="M174.5 463.397h295.714V184.125c0-54.766-43.763-104.093-104.093-104.093H174.5c-60.206 0-104.093 49.327-104.093 104.093v180.742c0.123 54.643 43.887 98.53 104.093 98.53z" fill="#F36A5A" p-id="10025"></path><path d="M952.852 364.744V184.125c0-54.766-43.764-104.093-104.094-104.093h-191.62c-54.767 0-104.094 43.764-104.094 104.093v284.712h295.714c60.33-5.44 104.094-49.327 104.094-104.093z" fill="#F1C40F" p-id="10026"></path><path d="M656.52 934.29h197.183c54.767 0 98.53-43.763 98.53-104.093V649.579c0-54.767-43.763-104.094-104.093-104.094H552.426v284.712c0 54.89 43.764 104.093 104.093 104.093z" fill="#45BE89" p-id="10027"></path><path d="M174.5 934.29h191.62c54.767 0 104.094-43.763 104.094-104.093V550.925H174.5c-54.766 0-104.093 43.764-104.093 104.093V835.76c0.123 49.327 43.887 98.53 104.093 98.53z" fill="#5491DE" p-id="10028"></path></svg>
-                </span>
-              </div>
-            </template>
-            <div class="flex flex-row flex-wrap max-w-md gap-4 p-2">
-              <div
-                class="
-                  flex flex-col
-                  gap-2
-                  overflow-hidden
-                  items-center
-                  justify-center
-                  cursor-pointer
-                  relative
-                  rounded
-                  hover:bg-gray-100
-                  dark:hover:bg-gray-800
-                  p-2
-                  transition
-                "
-                v-for="(app, index) in apps"
-                :key="index"
-                @click="target(app)"
-              >
-                <div
-                  class="w-10 h-10 text-white p-2 rounded"
-                  :style="{ 'background-color': app.color || '#1e5eff' }"
-                  v-if="app.icon"
-                  v-html="app.icon"
-                ></div>
-                <div class="truncate">{{ app.name }}</div>
-              </div>
+        <n-popover placement="right-end" trigger="click" :show-arrow="false">
+          <template #trigger>
+            <div
+              class="
+                cursor-pointer
+                rounded-sm
+                py-1.5
+                text-center
+                flex
+                items-center
+                justify-center
+                gap-1
+                hover:text-white hover:bg-gray-700
+                relative
+                mb-2
+              "
+            >
+              <span class="w-5 h-6 flex items-center justify-center">
+                <svg
+                  class="icon"
+                  viewBox="0 0 1024 1024"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                >
+                  <path
+                    d="M174.5 463.397h295.714V184.125c0-54.766-43.763-104.093-104.093-104.093H174.5c-60.206 0-104.093 49.327-104.093 104.093v180.742c0.123 54.643 43.887 98.53 104.093 98.53z"
+                    fill="#F36A5A"
+                    p-id="10025"
+                  ></path>
+                  <path
+                    d="M952.852 364.744V184.125c0-54.766-43.764-104.093-104.094-104.093h-191.62c-54.767 0-104.094 43.764-104.094 104.093v284.712h295.714c60.33-5.44 104.094-49.327 104.094-104.093z"
+                    fill="#F1C40F"
+                    p-id="10026"
+                  ></path>
+                  <path
+                    d="M656.52 934.29h197.183c54.767 0 98.53-43.763 98.53-104.093V649.579c0-54.767-43.763-104.094-104.093-104.094H552.426v284.712c0 54.89 43.764 104.093 104.093 104.093z"
+                    fill="#45BE89"
+                    p-id="10027"
+                  ></path>
+                  <path
+                    d="M174.5 934.29h191.62c54.767 0 104.094-43.763 104.094-104.093V550.925H174.5c-54.766 0-104.093 43.764-104.093 104.093V835.76c0.123 49.327 43.887 98.53 104.093 98.53z"
+                    fill="#5491DE"
+                    p-id="10028"
+                  ></path>
+                </svg>
+              </span>
             </div>
-          </n-popover>
+          </template>
+          <div class="flex flex-row flex-wrap max-w-md gap-4 p-2">
+            <div
+              class="
+                flex flex-col
+                gap-2
+                overflow-hidden
+                items-center
+                justify-center
+                cursor-pointer
+                relative
+                rounded
+                hover:bg-gray-100
+                dark:hover:bg-gray-800
+                p-2
+                transition
+              "
+              v-for="(app, index) in apps"
+              :key="index"
+              @click="target(app)"
+            >
+              <div
+                class="w-10 h-10 text-white p-2 rounded"
+                :style="{ 'background-color': app.color || '#1e5eff' }"
+                v-if="app.icon"
+                v-html="app.icon"
+              ></div>
+              <div class="truncate">{{ app.name }}</div>
+            </div>
+          </div>
+        </n-popover>
       </div>
     </div>
 
     <div class="lg:hidden h-14 z-10">
-      <div class="fixed bg-white dark:bg-gray-900 shadow w-full h-14 flex items-center gap-2">
+      <div
+        class="
+          fixed
+          bg-white
+          dark:bg-gray-900
+          shadow
+          w-full
+          h-14
+          flex
+          items-center
+          gap-2
+        "
+      >
         <div class="flex-none pl-4">
           <img class="w-6 h-6" :src="appInfo.logo || logo" />
         </div>
@@ -187,10 +368,11 @@
               items-center
               justify-center
               text-gray-400
-              hover:text-gray-500
-              hover:bg-gray-100
+              hover:text-gray-500 hover:bg-gray-100
               focus:outline-none
-              focus:ring-2 focus:ring-inset focus:ring-indigo-500
+              focus:ring-2
+              focus:ring-inset
+              focus:ring-indigo-500
             "
           >
             <svg
@@ -212,14 +394,15 @@
         </div>
       </div>
       <n-drawer v-model:show="mobileMenuShow" :width="502" placement="top">
-        <n-drawer-content title="斯通纳">
-          ...
-        </n-drawer-content>
+        <n-drawer-content title="斯通纳"> ... </n-drawer-content>
       </n-drawer>
     </div>
-
     <AppMenu :menu="menu[currentIndexs[0]]" :select="currentIndexs" />
-    <div class="flex-grow bg-gray-100 dark:bg-gray-900  dark:text-gray-200" id="page-animation">
+    <div
+      v-if="show"
+      class="flex-grow bg-gray-100 dark:bg-gray-900 dark:text-gray-200"
+      id="page-animation"
+    >
       <PageContent :currentUrl="currentUrl" :windowType="'page'" />
     </div>
   </div>
@@ -255,6 +438,7 @@ import logo from "../assets/images/logo.svg";
 
 export default {
   name: "Page",
+  props: ["show"],
   setup() {
     // 注册全局组件
     window.message = useMessage();
@@ -336,7 +520,7 @@ export default {
       // 用户信息
       userInfo: {},
       // 颜色模式
-      darkMode: window.localStorage.getItem('darkMode') === 'dark' ? 'dark' : 'light'
+      darkMode: localStorage.getItem("darkMode") === "dark" ? "dark" : "light",
     };
   },
   created() {
@@ -426,11 +610,6 @@ export default {
         }
       });
     },
-    // 切换颜色模式
-    switchDark() {
-      window.localStorage.setItem('darkMode', this.darkMode === 'dark' ? 'light' : 'dark')
-      window.location.reload()
-    }
   },
 };
 </script>
