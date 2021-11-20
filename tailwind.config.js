44d006ed399334137b679bde2328ae29b2026c36
xujian <908634674@qq.com>
2021-11-20 17:37:54 +0800

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
-const colors = {}
+const { light } = require('./arco-palette.json')
 
-
+const colors = Object.fromEntries(Object.keys(light).map(key => {
+  return [key, Object.fromEntries(light[key].map((val, index) => {
+    return [index === 0 ? 50 : index * 100, val]
+  }))]
+}))
 
 module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      colors: colors
+      colors
     },
   },
   variants: {
