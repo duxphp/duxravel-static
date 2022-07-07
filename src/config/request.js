bcb8d74a871bc9ed5b58d36e40f35a321d5c9d4a
xujian <908634674@qq.com>
2022-07-07 09:52:51 +0800

    修复ws报错

--- src/config/request.js
+++ src/config/request.js
@@ -1,6 +1,7 @@
 export default {
-  domain: 'http://192.168.1.107:8080',
+  domain: 'http://192.168.1.60',
   defaultModule: 'admin',
   // socket 仅调试模式生效
-  wsApi: '/admin/ws'
+  // wsApi: '/admin/ws',
+  wsApi: ''
 }
