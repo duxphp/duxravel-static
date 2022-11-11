346765c9c6140cdf0303ce844a84e447a071b665
mr.l <admin@duxphp.com>
2022-11-11 20:52:35 +0800

    update

--- src/config/request.js
+++ src/config/request.js
@@ -1,6 +1,6 @@
 export default {
-  domain: 'http://0.0.0.0:8080',
-  // domain: 'http://highway.test',
+  //domain: 'http://0.0.0.0:8080',
+  domain: 'http://yingjian_kaifa.test',
   defaultModule: 'admin',
   // socket 仅调试模式生效
   // wsApi: '/admin/ws',
