bff4d999196d5578aec543b39579d33c11553bd9
shockerli <shockerli@upfor.club>
2023-03-06 17:24:07 +0800

    新增与duxravel业务项目实时编译生效及使用文档

--- README.md
+++ README.md
@@ -51,3 +51,41 @@ yarn build
 ```
 yarn lint
 ```
+
+### 实时编译（Duxravel）
+本地开发时，如需 `duxravel-static` 与 `duxravel` 主项目（即您的业务项目）实时编译并生效，可按如下几步简便实现：
+
+1. 在您的业务项目 `composer.json` 添加如下配置
+
+```json
+    "repositories": {
+        "duxphp/duxravel-static": {
+            "type": "path",
+            "url": "/path/to/your/local/duxravel-static",
+            "options": {
+                "symlink": true
+            }
+        }
+    },
+```
+
+2. 在您的业务项目根目录下，执行 `composer update -vv` 或者 `composer require duxphp/duxravel-static -vv`
+
+3. 在 `duxravel-static` 创建 `.env.local` 文件并添加环境变量：
+
+```shell
+DUXRAVEL_PROJECT_ROOT = '/path/to/your/duxravel'
+```
+
+> `DUXRAVEL_PROJECT_ROOT` 值为您的业务项目根目录
+
+4. 在 `duxravel-static` 终端启动 `yarn build -w`
+
+终端输出如下日志，即表明成功。然后你可以任意修改前端源文件试试咯。
+```shell
+INFO  Publishing [duxravel] assets.  
+
+Copying directory [/path/to/github.com/duxphp/duxravel-static/dist/static/manage] to [public/static/manage] ........................ DONE
+Copying directory [/path/to/github.com/duxphp/duxravel-static/dist/static/tinymce] to [public/static/tinymce] ...................... DONE
+Copying file [/path/to/github.com/duxphp/duxravel-static/dist/manifest.json] to [public/static/manage-manifest.json] ............... DONE
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,60 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import command from 'rollup-plugin-command'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    dedupe: ['vue'],
-    alias: {
-      '@': '/src',
-      '@components': '/src/components'
-    }
-  },
-  build: {
-    assetsDir: 'static/manage',
-    manifest: true,
-    emptyOutDir: true,
+export default ({ mode }) => {
+  const env=loadEnv(mode, process.cwd(), '');
 
-    rollupOptions: {
-      // 覆盖默认的 .html 入口
-      input: 'src/main.js',
-      /*output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            let name = id.toString().split('node_modules/')[1].split('/')[0].toString()
-            if (name === 'tinymce' || name === 'apexcharts') {
-              return name
-            }else {
-              return 'vendors'
+  let plugins = [vue(), vueJsx()]
+  // 如果环境变量中设置有DUXRAVEL_PROJECT_ROOT，则执行Duxravel中的静态资源发布命令，将本static项目编译好的静态资源拷贝到业务主项目中
+  if (env.DUXRAVEL_PROJECT_ROOT) {
+    plugins.push(command(`cd ${env.DUXRAVEL_PROJECT_ROOT} && php artisan vendor:publish --tag="duxravel" --force`))
+  }
+
+  return defineConfig({
+    plugins: plugins,
+    resolve: {
+      dedupe: ['vue'],
+      alias: {
+        '@': '/src',
+        '@components': '/src/components'
+      }
+    },
+    build: {
+      assetsDir: 'static/manage',
+      manifest: true,
+      emptyOutDir: true,
+
+      rollupOptions: {
+        // 覆盖默认的 .html 入口
+        input: 'src/main.js',
+        /*output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              let name = id.toString().split('node_modules/')[1].split('/')[0].toString()
+              if (name === 'tinymce' || name === 'apexcharts') {
+                return name
+              }else {
+                return 'vendors'
+              }
             }
           }
-        }
-      }*/
-    }
-  },
-  server: {
-    // proxy: {
-    //   '/': {
-    //       target: 'http://dev.test/',
-    //       changeOrigin: true,
-    //       rewrite: (path) => path.replace(/^\/admin/, '')
+        }*/
+      }
+    },
+    server: {
+      // proxy: {
+      //   '/': {
+      //       target: 'http://dev.test/',
+      //       changeOrigin: true,
+      //       rewrite: (path) => path.replace(/^\/admin/, '')
 
-    //   }
-    // }
-    host: '0.0.0.0',
-    port: 3000
-  }
-})
+      //   }
+      // }
+      host: '0.0.0.0',
+      port: 3000
+    }
+  })
+}
